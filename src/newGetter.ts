128277a84c41aae730396a2477da07fb8878a2d8
VincentHui <VincentHui@hotmail.co.uk>
2020-08-14 15:16:49 +0100

    adding unimplemented newsGetter

---
+++ src/newGetter.ts
@@ -0,0 +1,30 @@
+export interface Article {
+  source: { name: string };
+  title: string;
+  url: string;
+  publishedAt: string;
+}
+
+export interface NewsData {
+  articles: Article[];
+  totalResults: number;
+}
+
+export interface Source {
+  id: string;
+  name: string;
+}
+const headlinesURL = "https://newsapi.org/v2/top-headlines";
+const apiKey = `b80f9d4a774344a3927c51efb69eb7d0`;
+export const GetNews = async (
+  pageSize: number = 5,
+  page: number = 1,
+  source: string = ""
+): Promise<NewsData> => {
+  throw Error("not implemented");
+};
+
+const sourceURL = "https://newsapi.org/v2/sources";
+export const GetSources = async (): Promise<Source[]> => {
+  throw Error("not implemented");
+};
