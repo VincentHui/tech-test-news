fd7e2fedccddb614fa3c03d40d40b7f38046cdd6
VincentHui <VincentHui@hotmail.co.uk>
2020-08-14 16:14:25 +0100

    starting to introduce drop down logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Source, Article, GetNews, GetSources } from "./newsGetter";
+import { SourceDropDown } from "./newsComponents";
+const defaultSource = { id: "", name: "All Sources" };
 
 function Widget() {
   const [newsState, setNews] = useState({
@@ -13,7 +15,17 @@ function Widget() {
       setSources({ sources });
     });
   }, []);
-  return <section></section>;
+  return (
+    <section>
+      <div>
+        <h1>News</h1>
+        <SourceDropDown
+          sources={[defaultSource].concat(sourceState.sources)}
+          onSourceChanged={() => {}}
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Widget;
